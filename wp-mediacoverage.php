7416bbc50cadcb09c2bd588fab37f609616b0e99
Stefano Maffulli <stefano.maffulli@scality.com>
2020-10-26 16:18:14 -0700

    Added meta box to capture URL, enabled featured image

--- wp-mediacoverage.php
+++ wp-mediacoverage.php
@@ -91,9 +91,9 @@ function custom_post_type() {
 	);
 	$args = array(
 		'label'                 => __( 'News mention', 'text_domain' ),
-		'description'           => __( 'LIst of mentions from media outlets.', 'text_domain' ),
+		'description'           => __( 'List of mentions from media outlets.', 'text_domain' ),
 		'labels'                => $labels,
-		'supports'              => array('title', 'editor', 'custom-fields' ),
+		'supports'              => array('title', 'editor', 'custom-fields', 'thumbnail' ),
 		'taxonomies'            => array( 'outlets' ),
 		'hierarchical'          => false,
 		'public'                => true,
@@ -117,7 +117,51 @@ add_action( 'init', 'custom_post_type', 0 );
 
 }
 
+/**
+ * Adds a meta box to the post editing screen
+ */
+function sm_custom_meta_url() {
+    add_meta_box( 'sm_meta', __( 'Add the URL of the media mention', 'sm-textdomain' ), 'sm_meta_callback', 'media-coverage' );
+}
+add_action( 'add_meta_boxes', 'sm_custom_meta_url' );
 
+/**
+ * Outputs the content of the meta box
+ */
+function sm_meta_callback( $post ) {
+    wp_nonce_field( basename( __FILE__ ), 'sm_nonce' );
+    $sm_stored_meta = get_post_meta( $post->ID );
+    ?>
+
+    <p>
+        <label for="coverage-url" class="sm-row-title"><?php _e( 'URL', 'sm-textdomain' )?></label>
+        <input size="105" type="text" name="coverage-url" id="coverage-url" value="<?php if ( isset ( $sm_stored_meta['coverage-url'] ) ) echo $sm_stored_meta['coverage-url'][0]; ?>" />
+    </p>
+
+    <?php
+}
+
+/**
+ * Saves the custom meta input
+ */
+function sm_meta_save( $post_id ) {
+ 
+    // Checks save status
+    $is_autosave = wp_is_post_autosave( $post_id );
+    $is_revision = wp_is_post_revision( $post_id );
+    $is_valid_nonce = ( isset( $_POST[ 'sm_nonce' ] ) && wp_verify_nonce( $_POST[ 'sm_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+    // Exits script depending on save status
+    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        return;
+    }
+
+    // Checks for input and sanitizes/saves if needed
+    if( isset( $_POST[ 'coverage-url' ] ) ) {
+        update_post_meta( $post_id, 'coverage-url', sanitize_text_field( $_POST[ 'coverage-url' ] ) );
+    }
+}
+add_action( 'save_post', 'sm_meta_save' );
 
 /* Stop Adding Functions Below this Line */
 ?>
